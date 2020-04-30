0526c143add24ff3c59917d0af7f1c5c28665af3
LEK QIU RONG <19036308@myrp.edu.sg>
2020-04-30 18:16:17 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19036308/registrationapp/q8.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id19036308.registrationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class q8 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_q8);
+    }
+}
